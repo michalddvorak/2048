45bd74a155d60ac2127709f6412ffb76491f8bee
Michal Dvořák <dvora125@fit.cvut.cz>
2025-12-31 01:26:56 +0100

    upd

--- utils/matrix.hpp
+++ utils/matrix.hpp
@@ -31,113 +31,111 @@ class matrix
 	
 	forward_row_iterator<matrix> row_forward_begin(size_t i)
 	{
-		return forward_row_iterator<matrix>(*this, i, 0);
+		return forward_row_iterator<matrix>(this, i, 0);
 	}
 	forward_column_iterator<matrix> column_forward_begin(size_t j)
 	{
-		return forward_column_iterator<matrix>(*this, 0, j);
+		return forward_column_iterator<matrix>(this, 0, j);
 	}
 	reverse_row_iterator<matrix> row_reverse_begin(size_t i)
 	{
-		return reverse_row_iterator<matrix>(*this, i, m_cols - 1);
+		return reverse_row_iterator<matrix>(this, i, m_cols - 1);
 	}
 	reverse_column_iterator<matrix> column_reverse_begin(size_t j)
 	{
-		return reverse_column_iterator<matrix>(*this, m_rows - 1, j);
+		return reverse_column_iterator<matrix>(this, m_rows - 1, j);
 	}
 	row_lr_iterator<matrix> row_lr_begin()
 	{
-		return row_lr_iterator<matrix>(*this, 0, 0);
+		return row_lr_iterator<matrix>(this, 0, 0);
 	}
 	
 	
 	forward_row_iterator<matrix> row_forward_end(size_t i)
 	{
-		return forward_row_iterator<matrix>(*this, i, m_cols);
+		return forward_row_iterator<matrix>(this, i, m_cols);
 	}
 	forward_column_iterator<matrix> column_forward_end(size_t j)
 	{
-		return forward_column_iterator<matrix>(*this, m_rows, j);
+		return forward_column_iterator<matrix>(this, m_rows, j);
 	}
 	reverse_row_iterator<matrix> row_reverse_end(size_t i)
 	{
-		return reverse_row_iterator<matrix>(*this, i, -1);
+		return reverse_row_iterator<matrix>(this, i, -1);
 	}
 	reverse_column_iterator<matrix> column_reverse_end(size_t j)
 	{
-		return reverse_column_iterator<matrix>(*this, -1, j);
+		return reverse_column_iterator<matrix>(this, -1, j);
 	}
 	row_lr_iterator<matrix> row_lr_end()
 	{
-		return row_lr_iterator<matrix>(*this, m_rows, 0);
+		return row_lr_iterator<matrix>(this, m_rows, 0);
 	}
 	
 	
 	const_forward_row_iterator<matrix> row_forward_begin(size_t i) const
 	{
-		return const_forward_row_iterator<matrix>(*this, i, 0);
+		return const_forward_row_iterator<matrix>(this, i, 0);
 	}
 	const_forward_column_iterator<matrix> column_forward_begin(size_t j) const
 	{
-		return const_forward_column_iterator<matrix>(*this, 0, j);
+		return const_forward_column_iterator<matrix>(this, 0, j);
 	}
 	const_reverse_row_iterator<matrix> row_reverse_begin(size_t i) const
 	{
-		return const_reverse_row_iterator<matrix>(*this, i, m_cols - 1);
+		return const_reverse_row_iterator<matrix>(this, i, m_cols - 1);
 	}
 	const_reverse_column_iterator<matrix> column_reverse_begin(size_t j) const
 	{
-		return const_reverse_column_iterator<matrix>(*this, m_rows - 1, j);
+		return const_reverse_column_iterator<matrix>(this, m_rows - 1, j);
 	}
 	const_row_lr_iterator<matrix> row_lr_begin() const
 	{
-		return const_row_lr_iterator<matrix>(*this, 0, 0);
+		return const_row_lr_iterator<matrix>(this, 0, 0);
 	}
 	
 	
 	const_forward_row_iterator<matrix> row_forward_end(size_t i) const
 	{
-		return const_forward_row_iterator<matrix>(*this, i, m_cols);
+		return const_forward_row_iterator<matrix>(this, i, m_cols);
 	}
 	const_forward_column_iterator<matrix> column_forward_end(size_t j) const
 	{
-		return const_forward_column_iterator<matrix>(*this, m_rows, j);
+		return const_forward_column_iterator<matrix>(this, m_rows, j);
 	}
 	const_reverse_row_iterator<matrix> row_reverse_end(size_t i) const
 	{
-		return const_reverse_row_iterator<matrix>(*this, i, -1);
+		return const_reverse_row_iterator<matrix>(this, i, -1);
 	}
 	const_reverse_column_iterator<matrix> column_reverse_end(size_t j) const
 	{
-		return const_reverse_column_iterator<matrix>(*this, -1, j);
+		return const_reverse_column_iterator<matrix>(this, -1, j);
 	}
 	const_row_lr_iterator<matrix> row_lr_end() const
 	{
-		return const_row_lr_iterator<matrix>(*this, m_rows, 0);
+		return const_row_lr_iterator<matrix>(this, m_rows, 0);
 	}
+ 
+	const_row_lr_iterator<matrix> begin() const
+	{
+		return row_lr_begin();
+	}
+	const_row_lr_iterator<matrix> end() const
+	{
+		return row_lr_end();
+	}
+    
+    row_lr_iterator<matrix> begin()
+    {
+        return row_lr_begin();
+    }
+    row_lr_iterator<matrix> end()
+    {
+        return row_lr_end();
+    }
 	
-	auto begin()
-	{
-		return row_lr_begin();
-	}
-	auto end()
-	{
-		return row_lr_end();
-	}
-	auto begin() const
-	{
-		return row_lr_begin();
-	}
-	auto end() const
-	{
-		return row_lr_end();
-	}
  private:
 	std::vector<std::vector<T>> m_arr;
 	size_t m_rows;
 	size_t m_cols;
 };
-
-
-
-
--- utils/matrix_iterator.hpp
+++ utils/matrix_iterator.hpp
@@ -3,221 +3,102 @@
 #include <memory>
 #include "matrix.hpp"
 
-template<typename Matrix>
+
+template <typename Self>
+class incrementable
+{
+  public:
+    template <typename ...Ts>
+    decltype(auto) operator++(Ts...)
+    {
+        std::conditional_t<sizeof...(Ts), Self, Self&> that = static_cast<Self&>(*this);
+        static_cast<Self*>(this)->do_increment();
+        return that;
+    }
+};
+
+
+template <typename Matrix>
 class matrix_iterator
 {
- public:
-	matrix_iterator(Matrix& matrix, size_t i, size_t j) : m_matrix(matrix),
-														  m_i(i),
-														  m_j(j) { }
-	typename Matrix::value_type& operator *()
-	{
-		return m_matrix[m_i][m_j];
-	}
-	friend bool operator ==(const matrix_iterator& x, const matrix_iterator& y)
-	{
-		return &x.m_matrix == &y.m_matrix && x.m_i == y.m_i && x.m_j == y.m_j;
-	}
-	friend bool operator !=(const matrix_iterator& x, const matrix_iterator& y)
-	{
-		return !(x == y);
-	}
- protected:
-	Matrix& m_matrix;
-	size_t m_i;
-	size_t m_j;
+  public:
+    matrix_iterator(Matrix* matrix, size_t i, size_t j) : m_matrix(matrix),
+                                                          m_i(i),
+                                                          m_j(j) { }
+    
+    typename Matrix::value_type& operator*() const
+    {
+        return (*m_matrix)[m_i][m_j];
+    }
+    
+    friend auto operator<=>(const matrix_iterator&, const matrix_iterator&) = default;
+  
+  protected:
+    Matrix* m_matrix;
+    size_t m_i;
+    size_t m_j;
 };
 
-template<typename Matrix>
+template <typename Matrix>
 class const_matrix_iterator
 {
- public:
-	const_matrix_iterator(const Matrix& matrix, size_t i, size_t j) : m_matrix(matrix),
-																	  m_i(i),
-																	  m_j(j) { }
-	const typename Matrix::value_type& operator *()
-	{
-		return m_matrix[m_i][m_j];
-	}
-	friend bool operator ==(const const_matrix_iterator& x, const const_matrix_iterator& y)
-	{
-		return &x.m_matrix == &y.m_matrix && x.m_i == y.m_i && x.m_j == y.m_j;
-	}
-	friend bool operator !=(const const_matrix_iterator& x, const const_matrix_iterator& y)
-	{
-		return !(x == y);
-	}
- protected:
-	const Matrix& m_matrix;
-	size_t m_i;
-	size_t m_j;
-};
-
-template<typename Matrix>
-class forward_column_iterator : public matrix_iterator<Matrix>
-{
- public:
-	forward_column_iterator(Matrix& matrix, size_t i, size_t j) : matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), forward_column_iterator, forward_column_iterator&> that = *this;
-		++this->m_i;
-		return that;
-	}
+  public:
+    const_matrix_iterator(const Matrix* matrix, size_t i, size_t j) : m_matrix(matrix),
+                                                                      m_i(i),
+                                                                      m_j(j) { }
+    
+    const typename Matrix::value_type& operator*() const
+    {
+        return (*m_matrix)[m_i][m_j];
+    }
+    
+    friend auto operator<=>(const const_matrix_iterator&, const const_matrix_iterator&) = default;
+  
+  protected:
+    const Matrix* m_matrix;
+    size_t m_i;
+    size_t m_j;
 };
 
 
-template<typename Matrix>
-class const_forward_column_iterator : public const_matrix_iterator<Matrix>
-{
- public:
-	const_forward_column_iterator(const Matrix& matrix, size_t i, size_t j) : const_matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), const_forward_column_iterator, const_forward_column_iterator&> that = *this;
-		++this->m_i;
-		return that;
-	}
-};
-
-template<typename Matrix>
-class forward_row_iterator : public matrix_iterator<Matrix>
-{
- public:
-	forward_row_iterator(Matrix& matrix, size_t i, size_t j) : matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), forward_row_iterator, forward_row_iterator&> that = *this;
-		++this->m_j;
-		return that;
-	}
-};
+#define DECLARE_MATRIX_ITERATOR(name, increment_logic) \
+template<typename Matrix> \
+class name : public matrix_iterator<Matrix>, public incrementable<name<Matrix>> \
+{ \
+   public:                          \
+    using matrix_iterator<Matrix>::matrix_iterator;                            \
+    void do_increment()          \
+        increment_logic                       \
+};                                  \
+template<typename Matrix> \
+class const_##name : public const_matrix_iterator<Matrix>, public incrementable<const_##name<Matrix>> \
+{ \
+   public:                          \
+    using const_matrix_iterator<Matrix>::const_matrix_iterator;                            \
+    void do_increment()        \
+        increment_logic                       \
+};                                  \
 
 
-template<typename Matrix>
-class const_forward_row_iterator : public const_matrix_iterator<Matrix>
-{
- public:
-	const_forward_row_iterator(const Matrix& matrix, size_t i, size_t j) : const_matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), const_forward_row_iterator, const_forward_row_iterator&> that = *this;
-		++this->m_j;
-		return that;
-	}
-};
+
+DECLARE_MATRIX_ITERATOR(forward_column_iterator, { ++this->m_i; })
 
 
-template<typename Matrix>
-class reverse_column_iterator : public matrix_iterator<Matrix>
-{
- public:
-	reverse_column_iterator(Matrix& matrix, size_t i, size_t j) : matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), reverse_column_iterator, reverse_column_iterator&> that = *this;
-		--this->m_i;
-		return that;
-	}
-};
 
+DECLARE_MATRIX_ITERATOR(forward_row_iterator, { ++this->m_j; })
 
-template<typename Matrix>
-class const_reverse_column_iterator : public const_matrix_iterator<Matrix>
-{
- public:
-	const_reverse_column_iterator(const Matrix& matrix, size_t i, size_t j) : const_matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), const_reverse_column_iterator, const_reverse_column_iterator&> that = *this;
-		--this->m_i;
-		return that;
-	}
-};
+DECLARE_MATRIX_ITERATOR(reverse_column_iterator, { --this->m_i; })
 
-template<typename Matrix>
-class reverse_row_iterator : public matrix_iterator<Matrix>
-{
- public:
-	reverse_row_iterator(Matrix& matrix, size_t i, size_t j) : matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), reverse_row_iterator, reverse_row_iterator&> that = *this;
-		--this->m_j;
-		return that;
-	}
-};
+DECLARE_MATRIX_ITERATOR(reverse_row_iterator, { --this->m_j; })
 
+DECLARE_MATRIX_ITERATOR(row_lr_iterator,
+                        {
+                     if (this->m_j == this->m_matrix->num_cols() - 1) {
+                         ++this->m_i;
+                         this->m_j = 0;
+                     }
+                     else
+                         ++this->m_j;
+                 }
+)
 
-template<typename Matrix>
-class const_reverse_row_iterator : public const_matrix_iterator<Matrix>
-{
- public:
-	const_reverse_row_iterator(const Matrix& matrix, size_t i, size_t j) : const_matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), const_reverse_row_iterator, const_reverse_row_iterator&> that = *this;
-		--this->m_j;
-		return that;
-	}
-};
-
-
-template<typename Matrix>
-class row_lr_iterator : public matrix_iterator<Matrix>
-{
- public:
-	row_lr_iterator(Matrix& matrix, size_t i, size_t j) : matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), row_lr_iterator, row_lr_iterator&> that = *this;
-		if(this->m_j == this->m_matrix.num_cols() - 1)
-		{
-			++this->m_i;
-			this->m_j = 0;
-		}
-		else
-			++this->m_j;
-		return that;
-	}
-};
-
-
-template<typename Matrix>
-class const_row_lr_iterator : public const_matrix_iterator<Matrix>
-{
- public:
-	const_row_lr_iterator(const Matrix& matrix, size_t i, size_t j) : const_matrix_iterator<Matrix>(matrix, i, j) { }
-	
-	template<typename ...Ts>
-	decltype(auto) operator ++(Ts...)
-	{
-		std::conditional_t<sizeof...(Ts), const_row_lr_iterator, const_row_lr_iterator&> that = *this;
-		if(this->m_j == this->m_matrix.num_cols() - 1)
-		{
-			++this->m_i;
-			this->m_j = 0;
-		}
-		else
-			++this->m_j;
-		return that;
-	}
-};
